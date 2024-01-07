06b68ecf13308e07a4a376d507db10783fdc4b4b
akmbo <67482112+akmbo@users.noreply.github.com>
2024-01-07 00:44:09 -0500

    first commit

---
+++ config/nvim/lua/vim-options.lua
@@ -0,0 +1,12 @@
+-- tab configuration
+vim.o.expandtab=true
+vim.o.tabstop=2
+vim.o.softtabstop=3
+vim.o.shiftwidth=2
+
+-- colorscheme
+vim.o.background = 'dark'
+vim.cmd([[highlight Normal guibg=none]])
+vim.cmd([[highlight NonText guibg=none]])
+vim.cmd([[highlight Normal ctermbg=none]])
+vim.cmd([[highlight NonText ctermbg=none]])
