d41cf432a9906756abc36a5eec8d981da30248dd
aaolen <aolenik@gmail.com>
2024-01-08 18:52:15 -0500

    early nvim config

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,23 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim", 
+    tag = "0.1.5",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      local builtin = require("telescope.builtin")
+      local function telescope_live_grep_open_files()
+        require("telescope.builtin").live_grep {
+          grep_open_files = true,
+          prompt_title = "Live Grep in Open Files",
+        }
+      end
+      vim.keymap.set("n", "<leader>s/", telescope_live_grep_open_files, {})
+      vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
     end
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").setup {
         extensions = {
@@ -21,5 +29,5 @@ return {
       }
       require("telescope").load_extension("ui-select")
     end
-  },
+  }
 }
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-  'nvim-treesitter/nvim-treesitter', 
-  build = ':TSUpdate',
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   config = function()
-    local config = require('nvim-treesitter.configs')
+    local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {'lua', 'javascript'},
+      ensure_installed = {"lua", "javascript"},
       highlight = { enable = true },
-      indent = { enable = true }
+      indent = { enable = true },
     })
   end
 }
+
