150995d1b2a9d39f1692950ac7672abe32492d9c
akmbo <67482112+akmbo@users.noreply.github.com>
2024-01-08 18:52:15 -0500

    early nvim config

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,12 @@
 return {
- 'nvim-lualine/lualine.nvim',
-  opts = {
-    { theme = 'dracula' }
-  }
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "dracula"
+      }
+    })
+  end
 }
+
