e238b7a397c7b918e73a84839fb5aeac7b6e0dcb
akmbo <67482112+akmbo@users.noreply.github.com>
2026-03-05 12:44:49 -0500

    Update fzf options

--- .bashrc.d/20-fzf.sh
+++ .bashrc.d/20-fzf.sh
@@ -1,2 +1,13 @@
-export FZF_DEFAULT_COMMAND='fd . --hidden --exclude ".git"'
+__preview='cat'
+if command -v bat >/dev/null 2>&1; then
+    __preview='bat -n --color=always --line-range=:1000 2>/dev/null'
+fi
+
+export FZF_DEFAULT_COMMAND='fd --strip-cwd-prefix --hidden --exclude .git'
+export FZF_CTRL_T_OPTS="
+    --walker-skip .git,node_modules,target
+    --preview '$__preview {}'
+    --height ~60%
+    --bind 'ctrl-/:change-preview-window(hidden|)'"
+
 eval "$(fzf --bash 2>/dev/null)"
